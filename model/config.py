4b0efa0a8321e591fd7377514850332048d8767a
Sangameswaran A <asangame@amazon.com>
2025-04-07 18:44:59 +0530

    Fixed "ValueError: invalid literal for int() with base 10: 'PROD'" in config.py

--- model/config.py
+++ model/config.py
@@ -1,4 +1,4 @@
 from os import environ
 
 MONGO_DB_URL = "mongodb+srv://prod-king:itsprodking@cluster0.gaeyh.mongodb.net"
-MONGO_DB_NAME = "fin-track" if int(environ.get("ENV")) == "PROD" else "fin-track-beta"
+MONGO_DB_NAME = "fin-track" if environ.get("ENV") == "PROD" else "fin-track-beta"
